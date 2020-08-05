67016d6f019f5a966920b073081a3d3863a5947b
RyoheiMashiko <RyoheiMashiko@users.noreply.github.com>
2020-08-05 16:41:26 +0900

    Updated.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-employee=[];
-const urlAPI = "https://randomuser.me/"
-const gridContainer = document.querySelector(".grid-countainer");
+employees=[];
+const urlAPI =  `https://randomuser.me/api/?results=12&inc=name, picture,
+email, location, phone, dob &noinfo &nat=US`;
+const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
+
 
 fetch(urlAPI) 
 .then(res => res.json())
@@ -13,7 +15,8 @@ fetch(urlAPI)
 
 function displayEmployees(employeeData) {
    let employees = employeeData; 
-   let employeeHTML = " ";
+   let employeeHTML = "";
+
    employees.forEach((employee,index) => {
    let name = employee.name;
    let email = employee.email;
@@ -26,7 +29,7 @@ function displayEmployees(employeeData) {
     <div class="text-container">
       <h2 class="name">${name.first} ${name.last}</h2>
       <p class="email">${email}</p>
-      <p class="adress">${adress}</p>
+      <p class="adress">${city}</p>
     </div>
   </div>`
    });
@@ -35,9 +38,8 @@ function displayEmployees(employeeData) {
 }
 
 function displayModel(index) {
-  let {name, dob, phoe, email, location:{
-      city, street, state, postcode}, picture }
-      = employeesHTML[index];
+  let { name, dob, phone, email, location: { city, street, state, postcode
+  }, picture } = employees[index];
   
   let date = new Date(dob.date);
 
@@ -57,8 +59,9 @@ function displayModel(index) {
 
   gridContainer.addEventListener("click", e => {
     if( e.target !== gridContainer) { 
+
     const card = e.target.closest(".card");
-    const index = card.attribute("data-index");
+    const index = card.getAttribute("data-index");
 
     displayModel(index);
     }
